29cb2b2337752d7daaa5e0318c4d37ab3e336298
VAK314 <vkochengin@gmail.com>
2018-05-19 00:00:13 -0400

    commit 1

---
+++ main.py
@@ -0,0 +1,11 @@
+class MainClass:
+    def __init__(self, sum_val):
+        self.val = sum_val
+    def get_triple(self):
+        result =[range(self.val)]
+        return result
+
+
+if __name__ =="__main__":
+    main_obj = MainClass(11)
+    print(main_obj.get_triple())
