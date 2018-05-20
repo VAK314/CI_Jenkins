8c06092170e6c0b9c197d77b2b030c18348e6ddb
VAK314 <vkochengin@gmail.com>
2018-05-20 02:19:06 -0400

    commit final
    Signed-off-by: VAK314 <vkochengin@gmail.com>

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ class MainClass:
             #if len(r):
             result.append(self.get_triple(i))
             '''print("i={} len={} |{}".format(i,len(result),result))
+            URA
             for r_ in result:
                 for rr_ in r_:
                     print("!{}!".format(sum(rr_)))
