d6e3731342bd7228e422846ae2c39a8326a8fa20
VAK314 <vkochengin@gmail.com>
2018-05-20 23:25:45 -0400

    commit final--good
    Signed-off-by: VAK314 <vkochengin@gmail.com>

--- test.py
+++ test.py
@@ -4,17 +4,29 @@ from main import MainClass
 
 
 class my_test(TestCase):
+    def setUp(self):
+        test_obj = MainClass(10, 'test_case')
+        self.result = test_obj.get_all()
 
     def test_summ(self):
-        test_obj = MainClass(10,'test_case')
-        result = test_obj.get_all()
-        for i in range(len(result)):
+        for i in range(len(self.result)):
             #self.assertEqual(0,0)
             if i<3:
-                self.assertEqual(0,len(result[i]))
+                self.assertEqual(0,len(self.result[i]))
             else:
-                for ii in result[i]:
+                for ii in self.result[i]:
                     self.assertEqual(i,sum(ii))
+    def test_un(self):
+        for i in range(3,len(self.result)):
+            c_ = 0
+            s_ = set()
+            for ii in self.result[i]:
+                c_ += 1
+                #r_=tuple(sorted(ii))
+                s_.add(tuple(sorted(ii)))
+            #print(s_,c_,len(s_))
+            self.assertEqual(c_, len(s_))
+
 
 if __name__=="__main__":
     unittest.main()
